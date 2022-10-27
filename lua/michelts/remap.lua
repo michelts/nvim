a9ea584ee83ef909c807f90c8e06a6e83694bc66
Michel Sabchuk <michel@turbosys.com.br>
2022-10-27 20:42:12 -0300

    Documment key mapping

--- lua/michelts/remap.lua
+++ lua/michelts/remap.lua
@@ -1,4 +1,7 @@
 local M = require('michelts.keymap')
 
+-- [O]pen the file's current directory
 M.nnoremap("<leader>o", "<cmd>Ex<CR>")
+
+-- Alt-Backspace to clear word-by-word in command line
 M.cmap('<a-bs>', '<c-w>')
